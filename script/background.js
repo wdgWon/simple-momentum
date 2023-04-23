d5bc98378d7e41f234ce8d4439d07048ec69df69
wdgWon <same666@naver.com>
2023-04-23 21:30:18 +0900

    간단한 컴포넌트 배치 및 배경화면 변경

--- script/background.js
+++ script/background.js
@@ -1,6 +1,6 @@
-const BGIMG_URL1 = 'https://cdn.pixabay.com/photo/2013/07/18/10/56/railroad-163518_960_720.jpg';
-const BGIMG_URL2 = 'https://cdn.pixabay.com/photo/2017/12/15/13/51/polynesia-3021072_960_720.jpg';
-const BGIMG_URL3 = 'https://cdn.pixabay.com/photo/2016/04/20/19/47/wolves-1341881_960_720.jpg';
+const BGIMG_URL1 = 'https://t1.daumcdn.net/cfile/tistory/99E3B33E5B2852742A';
+const BGIMG_URL2 = 'https://t1.daumcdn.net/cfile/tistory/176DCE484E95190E19';
+const BGIMG_URL3 = 'https://blog.kakaocdn.net/dn/cvXGS2/btqBW03RfsV/8ucvtATGWdo6PF3KK2PqiK/img.jpg';
 
 const img_arr = [BGIMG_URL1,BGIMG_URL2,BGIMG_URL3];
 
