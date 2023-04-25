ade2a9246f775cc567e7330041202c64fcb67048
wdgWon <same666@naver.com>
2023-04-25 23:27:15 +0900

    Feat, Change background by dblclick

--- script/background.js
+++ script/background.js
@@ -3,7 +3,14 @@ const BGIMG_URL2 = 'https://t1.daumcdn.net/cfile/tistory/176DCE484E95190E19';
 const BGIMG_URL3 = 'https://blog.kakaocdn.net/dn/cvXGS2/btqBW03RfsV/8ucvtATGWdo6PF3KK2PqiK/img.jpg';
 
 const img_arr = [BGIMG_URL1,BGIMG_URL2,BGIMG_URL3];
+const body = document.body;
 
-const background_imgtag = img_arr[Math.floor(Math.random() * img_arr.length)];
+setBackground();
 
-document.body.style.backgroundImage = `url(${background_imgtag})`;
+body.ondblclick = setBackground;
+
+
+function setBackground() {
+    const background_imgtag = img_arr[Math.floor(Math.random() * img_arr.length)];
+    body.style.backgroundImage = `url(${background_imgtag})`;
+}
