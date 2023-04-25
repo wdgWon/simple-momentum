8440338108d90799d05a32c47bc6dd63b2d86db2
wdgWon <same666@naver.com>
2023-04-25 23:37:16 +0900

    Style, Add underline component

--- script/clock.js
+++ script/clock.js
@@ -1,13 +1,20 @@
-const clock = document.querySelector('h2#clock');
-
+const clock = document.querySelector('#clock');
+const MERIDIEM = 'meridiem';
 
 function getClock() {
+    const isMeridiem = clock.classList.contains(MERIDIEM);
     const date = new Date();
-    const time_hour = String(date.getHours()).padStart(2,'0');
+    const full_hour = date.getHours();
+    const time_hour = String(full_hour%(isMeridiem?12:24)).padStart(2,'0');
     const time_minute = String(date.getMinutes()).padStart(2,'0');
-    const time_second = String(date.getSeconds()).padStart(2,'0');
-    clock.innerText = `${time_hour}:${time_minute}:${time_second}`;
+    const meridiem = isMeridiem ? full_hour>11 ? 'pm' : 'am' : '';
+    clock.innerHTML = `<span>${meridiem}</span>${time_hour}:${time_minute}`;
+}
+
+clock.onclick = () => {
+    clock.classList.toggle(MERIDIEM);
+    getClock();
 }
 
 getClock();
-setInterval(getClock, 1000);
+setInterval(getClock, 10000);
