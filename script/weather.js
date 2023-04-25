c6375f6f3bb183fcc78684f4d303bc7ba69131a4
wdgWon <same666@naver.com>
2023-04-25 23:36:03 +0900

    Feat, Enabled option of weather component

--- script/weather.js
+++ script/weather.js
@@ -1,15 +1,42 @@
 import {key as API_KEY} from '../weather-key.js'; 
+const description = document.querySelector('.weather-description');
+const span = document.querySelectorAll('.weather-description span');
+let count = 0;
+
+description.onclick = () => {
+    // const span = document.createElement('span');
+    // span.innerText = '123';
+    // description.appendChild(span);
+    // span[count%3].classList.toggle('hidden');
+    span[count%3].classList.add('disappear');
+    span[(count+1)%3].classList.remove('hidden');
+    setTimeout(() => span[(count+1)%3].classList.add('appear'), 100);
+    setTimeout(() => span[count++%3].className = 'hidden', 300);
+}
 
 function geoOn(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`;
+    console.log(url);
     fetch(url).then(response => response.json())
     .then(data => {
-        const weather = document.querySelector('#weather span:first-child');
-        const city = document.querySelector('#weather span:last-child');
-        weather.innerText = data.name;
-        city.innerText = data.weather[0].main;
+        const img = document.querySelector('#weather img');
+        const iconData = data.weather[0].icon;
+        img.setAttribute('src',`https://openweathermap.org/img/wn/${iconData}.png`); //icon 뒤에 @x2 붙이면 이미지 크기 2배
+
+        const cityData = data.name;
+        const tmpData = data.main.temp;
+        const descriptionData = data.weather[0].description;
+        const dataArray = [cityData,tmpData,descriptionData];
+        // for(let i in span) {
+        //     console.log(span);
+        // }
+        for(let i=0; i<span.length; i++) {
+            span[i].innerText = dataArray[i];
+            i ? span[i].classList.add('hidden') : span[i].classList.add('appear');
+        }
+        // span.innerText = `${cityData}\n${data.main.temp}\n${data.weather[0].description}`;
     })
 }
 
